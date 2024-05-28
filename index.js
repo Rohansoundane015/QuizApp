3ac601d1d381318a370b6584bd2d4d9a934bc3df
viraj <114827237+Rohansoundane015@users.noreply.github.com>
2024-05-28 13:41:09 +0530

    added quiz route

--- index.js
+++ index.js
@@ -1,10 +1,13 @@
-const express = require('express');
+const express = require('express')
 const app = express();
-const port = 3000;
-
+const port = 4000;
+const quizzes = require("./db/quizzes");
 app.get('/',(req,res)=>{
     res.send("hello geeks");
 })
-app.listen(port,()=>{
-    console.log(`Example app listening on port $}{port}`)
+app.get('/quiz',(res,req)=>{
+    res.send(quizzes);
+})
+app.listen(port||process.env.port,()=>{
+    console.log("Server is Ready... ");
 })
